005a9735d7184b9b9366d37bbfb1ecda7255cbb4
João Paulo <joaopaulozangrande@gmail.com>
2021-01-27 18:43:37 -0300

    I create a new Page(create).

--- resources/views/series/create.php
+++ resources/views/series/create.php
@@ -11,16 +11,17 @@
 <body>
 <div class="container">
     <div class="jumbotron">
-        <h1>Séries</h1>
+        <h1>Adicionar Série</h1>
     </div>
 
-    <a href="/series/criar" class="btn btn-dark mb-2">Adicionar</a>
+    <form action="" method="post">
+        <div class="form-group">
+            <label for="nome">Nome</label>
+            <input type="text" class="form-control" name="nome" id="nome">
+        </div>
+        <button class="btn btn-primary">Adicionar</button>
+    </form>
 
-    <ul class="list-group">
-    <?php foreach ($series as $serie): ?>
-        <li class="list-group-item"><?= $serie; ?></li>
-    <?php endforeach; ?>
-    </ul>
 </div>
 </body>
 </html>
