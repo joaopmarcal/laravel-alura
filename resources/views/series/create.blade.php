045d277aec15306bcf31573e3f4d59ec874a26ca
João Paulo <joaopaulozangrande@gmail.com>
2021-01-27 19:17:28 -0300

    i add a blade template.

---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,15 @@
+@extends('layout');
+
+@section('cabecalho')
+Adicionar Série
+@endsection
+
+@section('conteudo')
+<form action="" method="post">
+    <div class="form-group">
+        <label for="nome">Nome</label>
+        <input type="text" class="form-control" name="nome" id="nome">
+    </div>
+    <button class="btn btn-primary">Adicionar</button>
+</form>
+@endsection
