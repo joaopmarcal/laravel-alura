840a62a325955816c4f51ffbd1fa1c1db16bd85c
João Paulo <joaopaulozangrande@gmail.com>
2021-01-28 19:53:08 -0300

    I validate the request.

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -5,6 +5,16 @@ Adicionar Série
 @endsection
 
 @section('conteudo')
+@if($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach($errors->all() as $errors)
+                <li>{{ $errors }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 <form action="" method="post">
     @csrf
     <div class="form-group">
