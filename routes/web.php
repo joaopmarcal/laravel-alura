b683c90904c89b55066c7715f07606d92f44f004
João Paulo <joaopaulozangrande@gmail.com>
2021-02-05 19:38:21 -0300

    The system is now listening the episodes.

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::delete('/series/{id}', 'SeriesController@destroy');
 Route::post('/series/{id}/editaNome', 'SeriesController@editaNome');
 
 Route::get('/series/{serieId}/temporadas', 'TemporadasController@index');
+Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index');
