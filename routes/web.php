8691d6b5aaa79c353bcd76a725d7c80984df31b7
João Paulo <joaopaulozangrande@gmail.com>
2021-02-09 20:28:37 -0300

    middleware

--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,18 @@ Route::get('/series', 'SeriesController@index')
     ->name('listar_series');
 Route::get('/series/criar', 'SeriesController@create')
     ->name('form_criar_serie')
-    ->middleware('auth');
+    ->middleware('autenticador');
 Route::post('/series/criar', 'SeriesController@store')
-    ->middleware('auth');
+    ->middleware('autenticador');
 Route::delete('/series/{id}', 'SeriesController@destroy')
-    ->middleware('auth');
+    ->middleware('autenticador');
 Route::post('/series/{id}/editaNome', 'SeriesController@editaNome')
-    ->middleware('auth');
+    ->middleware('autenticador');
 
 Route::get('/series/{serieId}/temporadas', 'TemporadasController@index');
 Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index');
 Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir')
-    ->middleware('auth');
+    ->middleware('autenticador');
 
 Auth::routes();
 
