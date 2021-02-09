e70ea2ef3ae0624e2ca6d3b80413b429f69f8a6b
João Paulo <joaopaulozangrande@gmail.com>
2021-02-09 19:20:24 -0300

    I protected the necessary.

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -7,7 +7,9 @@ Séries
 @section('conteudo')
 @include('mensagem', ['mensagem' => $mensagem])
 
+@auth
 <a href="{{ route('form_criar_serie') }}" class="btn btn-dark mb-2">Adicionar</a>
+@endauth
 
 <ul class="list-group">
     @foreach($series as $serie)
@@ -25,12 +27,15 @@ Séries
         </div>
 
         <span class="d-flex">
+            @auth
             <button class="btn btn-info btn-sm mr-1" onclick="toggleInput({{ $serie->id }})">
                 <i class="fas fa-edit"></i>
             </button>
+            @endauth
             <a href="/series/{{$serie->id}}/temporadas" class="btn btn-info btn-sm mr-1 align-self-center">
                 <i class="fas fa-external-link-alt"></i>
             </a>
+            @auth
             <form class="align-items-center" method="post" action="/series/{{ $serie->id }}"
                   onsubmit="return confirm('Tem certeza que deseja remover {{ addslashes($serie->nome) }}?')">
                 @csrf
@@ -39,6 +44,7 @@ Séries
                     <i class="far fa-trash-alt"></i>
                 </button>
             </form>
+            @endauth
         </span>
     </li>
     @endforeach
