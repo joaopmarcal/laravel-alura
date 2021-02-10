5b4ef8da9996efa917af24cb2f338444d1230d81
João Paulo <joaopaulozangrande@gmail.com>
2021-02-10 19:19:45 -0300

    I made an test environment to my database.

--- tests/Feature/CriadorDeSerieTest.php
+++ tests/Feature/CriadorDeSerieTest.php
@@ -10,6 +10,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CriadorDeSerieTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function testCriarSerie()
     {
         $criadorDeSerie = new CriadorDeSerie();
